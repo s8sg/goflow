00a4d50bed941783bfb78f9464a1088dbfb03dda
Dan <danash001@gmail.com>
2023-04-27 01:00:08 +0300

    Replace httprouter with gin

--- runtime/new_request_handler_wrapper.go
+++ runtime/new_request_handler_wrapper.go
@@ -7,27 +7,27 @@ import (
 
 	runtimepkg "github.com/s8sg/goflow/core/runtime"
 
+	"github.com/gin-gonic/gin"
 	"github.com/s8sg/goflow/core/sdk/executor"
-	"github.com/julienschmidt/httprouter"
 )
 
-func newRequestHandlerWrapper(runtime runtimepkg.Runtime, handler func(*runtimepkg.Response, *runtimepkg.Request, executor.Executor) error) func(http.ResponseWriter, *http.Request, httprouter.Params) {
-	return func(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
-		id := params.ByName("id")
-		flowName := params.ByName("flowName")
+func newRequestHandlerWrapper(runtime runtimepkg.Runtime, handler func(*runtimepkg.Response, *runtimepkg.Request, executor.Executor) error) func(*gin.Context) {
+	fn := func(c *gin.Context) {
+		id := c.Param("id")
+		flowName := c.Param("flowName")
 
-		body, err := ioutil.ReadAll(req.Body)
+		body, err := ioutil.ReadAll(c.Request.Body)
 		if err != nil {
-			handleError(w, fmt.Sprintf("failed to execute request, "+err.Error()))
+			handleError(c.Writer, fmt.Sprintf("failed to execute request, "+err.Error()))
 			return
 		}
 
 		reqParams := make(map[string][]string)
-		for _, param := range params {
+		for _, param := range c.Params {
 			reqParams[param.Key] = []string{param.Value}
 		}
 
-		for key, values := range req.URL.Query() {
+		for key, values := range c.Request.URL.Query() {
 			reqParams[key] = values
 		}
 
@@ -36,31 +36,33 @@ func newRequestHandlerWrapper(runtime runtimepkg.Runtime, handler func(*runtimep
 		response.Header = make(map[string][]string)
 		request := &runtimepkg.Request{
 			Body:      body,
-			Header:    req.Header,
+			Header:    c.Request.Header,
 			FlowName:  flowName,
 			RequestID: id,
 			Query:     reqParams,
-			RawQuery:  req.URL.RawQuery,
+			RawQuery:  c.Request.URL.RawQuery,
 		}
 
 		ex, err := runtime.CreateExecutor(request)
 		if err != nil {
-			handleError(w, fmt.Sprintf("failed to execute request, "+err.Error()))
+			handleError(c.Writer, fmt.Sprintf("failed to execute request, "+err.Error()))
 			return
 		}
 
 		err = handler(response, request, ex)
 		if err != nil {
-			handleError(w, fmt.Sprintf("request failed to be processed, "+err.Error()))
+			handleError(c.Writer, fmt.Sprintf("request failed to be processed, "+err.Error()))
 			return
 		}
 
-		headers := w.Header()
+		headers := c.Writer.Header()
 		for key, values := range response.Header {
 			headers[key] = values
 		}
 
-		w.WriteHeader(http.StatusOK)
-		w.Write(response.Body)
+		c.Writer.WriteHeader(http.StatusOK)
+		c.Writer.Write(response.Body)
 	}
+
+	return fn
 }
