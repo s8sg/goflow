f125391b023d1b96f8df0a7966052399a213b5e8
s8sg <swarvanusg@gmail.com>
2023-06-22 12:51:04 +0700

    Add new variable for RedisPassword
    Signed-off-by: s8sg <swarvanusg@gmail.com>

--- samples/examples.go
+++ samples/examples.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/s8sg/goflow/samples/condition"
 	"github.com/s8sg/goflow/samples/loop"
+	"github.com/s8sg/goflow/samples/myflow"
 	"github.com/s8sg/goflow/samples/parallel"
 	"github.com/s8sg/goflow/samples/serial"
 	"github.com/s8sg/goflow/samples/single"
@@ -15,6 +16,7 @@ func main() {
 	fs := &goflow.FlowService{
 		Port:              8080,
 		RedisURL:          "localhost:6379",
+		RedisPassword:     "redis",
 		OpenTraceUrl:      "localhost:5775",
 		WorkerConcurrency: 5,
 		EnableMonitoring:  true,
@@ -25,5 +27,6 @@ func main() {
 	fs.Register("parallel", parallel.DefineWorkflow)
 	fs.Register("condition", condition.DefineWorkflow)
 	fs.Register("loop", loop.DefineWorkflow)
+	fs.Register("myflow", myflow.DefineWorkflow)
 	fmt.Println(fs.Start())
 }
